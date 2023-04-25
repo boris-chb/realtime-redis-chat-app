4f057b799e9573b1f41aa05cd89683feb7fdaf1a
Boris <47457848+boris-chb@users.noreply.github.com>
2023-04-25 12:30:57 +0100

    Button component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from '@/components/UI/Button';
+import { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <div>
+      <Button>bro</Button>
+    </div>
+  );
+};
+
+export default page;
